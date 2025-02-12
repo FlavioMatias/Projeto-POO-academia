8805018827a1471ba7559584f8e16126e45dce18
icaro <164355795+icarolindogostoso@users.noreply.github.com>
2025-02-12 01:17:23 +0000

    removendo a pasta teste

--- teste/src/Clientes.java
+++
@@ -1,6 +0,0 @@
-public class Clientes extends Modelo<Cliente> {
-    @Override
-    public void imprimir() {
-        System.out.println("Lista de Clientes");
-    }
-}
--- teste/src/TemNome.java
+++
@@ -1,3 +0,0 @@
-public interface TemNome {
-    String getNome();
-}
