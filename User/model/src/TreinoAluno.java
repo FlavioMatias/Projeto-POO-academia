97fd7672ba7e92ba664d513eac63a455d7939509
icaro <164355795+icarolindogostoso@users.noreply.github.com>
2025-02-18 20:02:39 -0300

    adicao da variavel para saber se o treinoaluno está ativo

--- User/model/src/TreinoAluno.java
+++ User/model/src/TreinoAluno.java
@@ -8,13 +8,13 @@ public class TreinoAluno implements Inter{
     private int id;
     private int id_aluno;
     private String data;
-    private String data_final;
+    private boolean ativa;
 
-    public TreinoAluno(int id, int id_aluno, String data, String data_final) {
+    public TreinoAluno(int id, int id_aluno, String data, boolean ativa) {
         setId(id);
         setIdAluno(id_aluno);
         setData(data);
-        setDataFinal(data_final);
+        setAtiva(ativa);
     }
 
     public void setId(int id) {
@@ -47,24 +47,12 @@ public class TreinoAluno implements Inter{
         }
     }
 
-    public void setDataFinal(String data_final) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        try {
-            LocalDate dataObj = LocalDate.parse(data_final, formatter);
-
-            if (dataObj.isAfter(LocalDate.now())) {
-                throw new IllegalArgumentException("Data final nao pode ser no futuro.");
-            }
-
-            if (dataObj.isBefore(LocalDate.parse(data, formatter))) {
-                throw new IllegalArgumentException("A data final nao pode ser anterior a data de inicio.");
-            }
-
-            this.data_final = data_final;
-        } catch (DateTimeParseException e) {
-            throw new IllegalArgumentException("A data final eh invalida. O formato correto eh dd/MM/yyyy.");
+    public void setAtiva(boolean ativa) {
+        if(!(ativa == true || ativa == false)) {
+            throw new IllegalArgumentException("O campo 'ativa' deve ser um valor booleano (true ou false).");
         }
+
+        this.ativa = ativa;
     }
 
     public int getId() {
@@ -79,8 +67,8 @@ public class TreinoAluno implements Inter{
         return this.data;
     }
 
-    public String getDataFinal() {
-        return this.data_final;
+    public boolean getAtiva() {
+        return this.ativa;
     }
 
     public Map<String, Object> toMap() {
@@ -88,12 +76,12 @@ public class TreinoAluno implements Inter{
         map.put("id", id);
         map.put("id_aluno", id_aluno);
         map.put("data", data);
-        map.put("data_final", data_final);
+        map.put("ativa", ativa);
         return map;
     }
 
     @Override
     public String toString() {
-        return "TreinoAluno(ID: " + this.id + ", ID Aluno: " + this.id_aluno + ", Data: " + this.data + ", Data Final: " + this.data_final + ")";
+        return "TreinoAluno(ID: " + this.id + ", ID Aluno: " + this.id_aluno + ", Data: " + this.data + ", Ativo: " + this.ativa + ")";
     }
 }
